46d844671cb391a8d90bd56ad7d8fc8cc088c32a
sruthybabu <sruthybabu64@gmail.com>
2022-05-12 09:37:30 +0530

    book application completed

---
+++ bookmanage.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <h1>SEARCH BOOK</h1>
+                <table class="table table-borderless">
+                    <tr>
+                        <td>Book Name</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button class="btn btn-success">SEARCH</button></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <h2>EDIT BOOK</h2>
+                <table class="table table-borderless">
+                    <tr>
+                        <td>Book Name</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button class="btn btn-success">SEARCH</button></td>
+                    </tr>
+                    <tr>
+                        <td>Author</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td>Price</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button class="btn btn-success">EDIT</button></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="col col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <h3>DELETE BOOK</h3>
+                <table class="table table-borderless">
+                    <tr>
+                        <td>Book Name</td>
+                        <td><input type="text" class="form-control"></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><button class="btn btn-success">DELETE</button></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ page.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col col-12 col-sm-2 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non maxime quasi voluptates, possimus ex qui officiis ratione ducimus soluta excepturi dignissimos tempore quis perspiciatis magni! Similique, expedita facilis! Beatae, ad.</p>
+            </div>
+            
+            <div class="col col-12 col-sm-8 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum soluta maiores, iure cupiditate suscipit mollitia sint unde magnam adipisci nam hic, accusamus laboriosam laudantium dolorem at? Architecto excepturi tempore laudantium!</p>
+            </div>
+            
+            <div class="col col-12 col-sm-2 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque, quo, quidem ipsam aperiam possimus deleniti fuga quos est inventore molestiae explicabo temporibus ipsa blanditiis atque velit, labore harum rem? Eius!</p>
+            </div>
+
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
